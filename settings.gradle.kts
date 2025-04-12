8731a80a3f4c9e52890421f2cc540eaf076f3f60
andihasan97 <andihasanelfalakiy@gmail.com>
2025-04-12 10:28:41 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "belajar-java-dasar-pzn"
+
---
+++ src/main/java/org/andihasan7/Main.java
@@ -0,0 +1,7 @@
+package org.andihasan7;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
